87ff161decdfdca0217173930e4110f9d3f9e44c
boom <zhuravsaki@gmail.com>
2021-04-20 02:05:36 +0300

    Small refactoring

--- signupinsite/settings.py
+++ signupinsite/settings.py
@@ -18,9 +18,8 @@ import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# This is new:
+
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
@@ -91,13 +90,6 @@ WSGI_APPLICATION = 'signupinsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
